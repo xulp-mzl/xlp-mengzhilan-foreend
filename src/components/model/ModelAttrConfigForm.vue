060a7b8191f7059cce0d9515f8088758074cb105
xulongp <15179220203@163.com>
2022-07-23 00:25:35 +0800

    功能开发

--- src/components/model/ModelAttrConfigForm.vue
+++ src/components/model/ModelAttrConfigForm.vue
@@ -12,35 +12,200 @@
     </template>
     <template #body>
       <div class="attr-body">
-        <el-form :model="modelAttrInfo" label-width="100px" ref="form" size="mini">
+        <el-form :model="modelAttrInfo" label-width="112px" ref="form" size="mini">
           <div class="attr-title">基础属性</div>
           <el-row :gutter="5">
             <el-col :span="8">
-              <el-form-item label="模型名称："
-                            prop="attrName"
-                            :rules="[
-                { required: true, message: '模型名称不能为空'},
-              ]"
+              <el-form-item
+                  label="属性id："
+              >
+                <el-input v-model="modelAttrInfo.attrId" disabled class="custom-input" type="mini"></el-input>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="模型名称："
+                  prop="attrName"
+                  :rules="[
+                    { required: true, message: '模型名称不能为空'},
+                  ]"
               >
                 <el-input v-model.trim="modelAttrInfo.attrName" autocomplete="off" placeholder="请输入属性名称" class="custom-input" type="mini"></el-input>
               </el-form-item>
             </el-col>
+
             <el-col :span="8">
               <el-form-item
                   label="排序号："
                   prop="orderNo"
-                  :rules="[{ type: 'number', message: '排序号必须为数字值'}]"
+                  :rules="[
+                    { required: true, message: '排序号不能为空'},
+                    { type: 'number', message: '排序号必须为数字值'}
+                  ]"
               >
                 <el-input v-model.number="modelAttrInfo.orderNo" autocomplete="off" class="custom-input" type="mini"></el-input>
               </el-form-item>
             </el-col>
+          </el-row>
+
+          <div class="attr-title">其他配置</div>
+
+          <el-row :gutter="5">
             <el-col :span="8">
               <el-form-item
-                  label="排序号："
-                  prop="orderNo"
-                  :rules="[{ type: 'number', message: '排序号必须为数字值'}]"
+                  label="新增时可编辑："
+                  prop="adding"
               >
-                <el-input v-model.number="modelAttrInfo.orderNo" autocomplete="off" class="custom-input" type="mini"></el-input>
+                <el-radio-group v-model="modelAttrInfo.adding" type="mini" @change="addingChange">
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="新增时可显示："
+                  prop="addingShow"
+              >
+                <el-radio-group v-model="modelAttrInfo.addingShow" type="mini">
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="提示信息："
+                  prop="placeholder"
+              >
+                <el-input v-model="modelAttrInfo.placeholder" autocomplete="off" class="custom-input" type="mini" :disabled="disabled"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row :gutter="5">
+            <el-col :span="8">
+              <el-form-item
+                  label="编辑时可编辑："
+                  prop="edit"
+              >
+                <el-radio-group v-model="modelAttrInfo.edit" type="mini" @change="editChange">
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="编辑时可显示："
+                  prop="editShow"
+              >
+                <el-radio-group v-model="modelAttrInfo.editShow" type="mini">
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="是否必填："
+                  prop="required"
+              >
+                <el-radio-group v-model="modelAttrInfo.required" type="mini" :disabled="disabled">
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row :gutter="5">
+            <el-col :span="8">
+              <el-form-item
+                  label="自定义输入框："
+                  prop="slot"
+              >
+                <el-radio-group v-model="modelAttrInfo.slot" type="mini" @change="slotChange">
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="自定义类名："
+                  prop="itemClassName"
+              >
+                <el-input v-model.number="modelAttrInfo.itemClassName"
+                          autocomplete="off"
+                          class="custom-input" type="mini">
+                </el-input>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="文本长度："
+                  prop="textMaxLength"
+                  :rules="[
+                    validateInt('文本长度必须是不小于0的整数！')
+                  ]"
+              >
+                <el-input v-model.number="modelAttrInfo.textMaxLength"
+                          autocomplete="off"
+                          class="custom-input"
+                          type="mini"
+                          :disabled="disabled">
+                </el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row :gutter="5" v-if="!disabled">
+            <el-col :span="8">
+              <el-form-item
+                  label="输入框类型："
+                  prop="formInputType"
+              >
+                <el-radio-group v-model="modelAttrInfo.formInputType" type="mini" >
+                  <el-radio :label="true">是</el-radio>
+                  <el-radio :label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="自定义类名："
+                  prop="itemClassName"
+              >
+                <el-input v-model.number="modelAttrInfo.itemClassName"
+                          autocomplete="off"
+                          class="custom-input" type="mini">
+                </el-input>
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="8">
+              <el-form-item
+                  label="文本长度："
+                  prop="textMaxLength"
+                  :rules="[
+                    validateInt('文本长度必须是不小于0的整数！')
+                  ]"
+              >
+                <el-input v-model.number="modelAttrInfo.textMaxLength"
+                          autocomplete="off"
+                          class="custom-input"
+                          type="mini"
+                          :disabled="disabled">
+                </el-input>
               </el-form-item>
             </el-col>
           </el-row>
@@ -55,10 +220,11 @@
 import FormMixins from '@/components/mixins/form/FormMixins'
 import CommonDialog from '@/components/common/CommonDialog'
 import {getModelAttr} from '@/js/api/modelAttr'
+import RuleValidation from '@/components/mixins/rules/RuleValidation'
 
 export default {
   name: 'ModelAttrConfigForm',
-  mixins: [FormMixins],
+  mixins: [FormMixins, RuleValidation],
   components: {
     CommonDialog
   },
@@ -88,7 +254,8 @@ export default {
   data(){
     return {
       loading: true,
-      modelAttrInfo: {}
+      modelAttrInfo: {},
+      disabled: false
     }
   },
   methods: {
@@ -103,10 +270,38 @@ export default {
         if (!this.modelAttrInfo.attrName){
           this.modelAttrInfo.attrName = this.attrName
         }
+        if (!this.modelAttrInfo.attrId){
+          this.modelAttrInfo.attrId = this.attrId
+        }
+        if (this.modelAttrInfo.slot === true){
+          this.disabled = true
+          this.modelAttrInfo.textMaxLength = 0
+        }
         console.log(this.modelAttrInfo)
       }
       this.loading = false
       appLoading.close()
+    },
+    addingChange(val){
+      if (val === true){
+        this.modelAttrInfo.addingShow = val
+      }
+    },
+    editChange(val){
+      if (val === true){
+        this.modelAttrInfo.editShow = val
+      }
+    },
+    slotChange(val){
+      this.disabled = val
+      if (val){
+        this.modelAttrInfo.required = false
+        this.modelAttrInfo.placeholder = ''
+        this.modelAttrInfo.textMaxLength = 0
+        this.$refs.form.clearValidate('textMaxLength')
+      } else {
+
+      }
     }
   },
   mounted(){
